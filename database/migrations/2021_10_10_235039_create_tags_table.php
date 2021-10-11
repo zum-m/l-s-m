e95256798b21a792454eb37ee4e86d9624ea08d0
zum-m <21039m@gmail.com>
2021-10-11 09:39:18 +0900

    マイグレーションファイルにテーブル定義を実装

--- database/migrations/2021_10_10_235039_create_tags_table.php
+++ database/migrations/2021_10_10_235039_create_tags_table.php
@@ -14,8 +14,15 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('id', true);
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->softDeletes();
+            // timestampと書いてしまうと、レコード挿入時、更新時に値が入らないので、DB::rawで直接書いてます
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            // 🟡ユーザーIDの規制とは？
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
